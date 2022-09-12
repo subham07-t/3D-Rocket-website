b645b1dd9aa341b58da39945c7ea1088c7225bec
subham07-t <82716446+subham07-t@users.noreply.github.com>
2022-09-13 00:31:05 +0530

    rocket added

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Spline from "@splinetool/react-spline";
+import GitHubIcon from "@mui/icons-material/GitHub";
+import LinkedInIcon from "@mui/icons-material/LinkedIn";
+import TwitterIcon from "@mui/icons-material/Twitter";
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="container">
+        <div className="navbar">
+          <div className="logo">
+            <img src="/imgs/channel-11.jpeg" alt="" />
+          </div>
+          <ul className="option">
+            <li>Home</li>
+            <li>About</li>
+            <li>Projects</li>
+            <li>Contact</li>
+          </ul>
+        </div>
+        <div className="container-body">
+          <div className="right">
+            <div>
+              <h1>Dattebayo!Keep Going</h1>
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Est,
+                nesciunt quaerat voluptatibus magni aliquid at.
+              </p>
+            </div>
+            <div className="right-bot">
+              <LinkedInIcon fontSize="large" />
+              <GitHubIcon fontSize="large" />
+              <TwitterIcon fontSize="large" />
+            </div>
+          </div>
+
+          <Spline
+            className="left"
+            scene="https://prod.spline.design/XJECSOXYRvwLFJqs/scene.splinecode"
+          />
+        </div>
+      </div>
+    </>
   );
 }
-
-export default App;
